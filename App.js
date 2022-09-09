aab1ce8396ed2678050cba503bd68daf68c66553
Sashankr <sashankr97@gmail.com>
2022-09-09 19:20:40 +0530

    added snap functionality

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, ScrollView, View } from "react-native";
 import CarsList from "./components/CarsList";
-
+import Header from "./components/Header";
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarsList />
     </View>
   );
