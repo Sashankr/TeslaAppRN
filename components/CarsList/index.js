c73667e85837e8643e73bca1a3ca8af9976315aa
Sashankr <sashankr97@gmail.com>
2022-09-09 18:23:36 +0530

    Render list of cars

---
+++ components/CarsList/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { View, Text, FlatList } from "react-native";
+import styles from "./styles";
+import cars from "./cars";
+
+import CarItem from "../CarItem";
+
+const CarsList = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => {
+          return <CarItem car={item} />;
+        }}
+      />
+    </View>
+  );
+};
+
+export default CarsList;
