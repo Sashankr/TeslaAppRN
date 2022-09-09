09a64fd41d3e4eb14b2f979ade410f1e88d5657e
Sashankr <sashankr97@gmail.com>
2022-09-09 18:27:47 +0530

    snap effect

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Dimensions } from "react-native";
 import styles from "./styles";
 import cars from "./cars";
 
@@ -14,6 +14,10 @@ const CarsList = () => {
         renderItem={({ item }) => {
           return <CarItem car={item} />;
         }}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment={"start"}
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").height}
       />
     </View>
   );
